32083600ab77aa5eac2f8f09ccc397d92006d595
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2023-12-01 22:50:20 +0100

    contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid'
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: ''
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { name, contacts, number } = this.state;
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+     
+  };
+
+  render() {
+    const { name, contacts, number } = this.state;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <label>
+            Number:
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>{contact.name} - {contact.number}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+export default App;
